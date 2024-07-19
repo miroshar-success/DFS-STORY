c7158aab109c4fd29dba239beb512ea77c16200f
leom0311 <milosfilipovic0311@gmail.com>
2024-07-19 16:06:14 +0200

    feat: vercel.json

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+const express = require("express");
 const multer = require("multer");
 const fs = require("fs");
 const path = require("path");
 const { Story } = require("inkjs");
-const util = require("util");
+const cors = require("cors");
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+const app = express();
+const port = 3000;
 
+// Set up multer for file upload
+const upload = multer({ dest: "uploads/" });
+
+// Function to log memory usage
 const logMemoryUsage = () => {
   const used = process.memoryUsage();
   console.log("Memory usage:");
@@ -17,32 +22,42 @@ const logMemoryUsage = () => {
   }
 };
 
+// Function to process JSON content
 const recursionDFS = (story) => {
   while (story.canContinue) {
     const line = story.Continue();
-    const backUpJson = story.state.toJson();
 
+    // console.log(line);
+    backUpJson = story.state.toJson();
+    // console.log(backUpJson);
     for (let i = 0; i < story.currentChoices.length; i++) {
       let choice = story.currentChoices[i];
       story.ChooseChoiceIndex(choice.index);
       recursionDFS(story);
+
       logMemoryUsage();
       story.state.LoadJson(backUpJson);
     }
   }
 };
 
-module.exports = (req, res) => {
-  upload.single("file")(req, res, (err) => {
+app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
+
+// API to upload a JSON file
+app.post("/upload", upload.single("file"), (req, res) => {
+  const filePath = path.join(__dirname, req.file.path);
+
+  // Read the JSON file
+  fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
-      console.error("Error uploading file:", err);
-      return res.status(500).send("Error uploading file");
+      console.error("Error reading file:", err);
+      return res.status(500).send("Error reading file");
     }
 
+    // Parse the JSON content
     try {
-      const inkJson = JSON.parse(
-        req.file.buffer.toString("utf8").replace(/^\uFEFF/, "")
-      );
+      const inkJson = JSON.parse(data.replace(/^\uFEFF/, ""));
       const story = new Story(inkJson);
       recursionDFS(story);
 
@@ -50,6 +65,16 @@ module.exports = (req, res) => {
     } catch (parseErr) {
       console.error("Error parsing JSON:", parseErr);
       res.status(400).send("Invalid JSON format");
+    } finally {
+      fs.unlink(filePath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error("Error deleting file:", unlinkErr);
+        }
+      });
     }
   });
-};
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
